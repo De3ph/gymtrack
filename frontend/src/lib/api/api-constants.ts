9867bd9f7cd9c11ab1967b51fb32815dd80dd9dc
Hamit Can <62203579+De3ph@users.noreply.github.com>
2026-08-16 22:30:00 +0300

    Add API stale time constants

---
+++ frontend/src/lib/api/api-constants.ts
@@ -0,0 +1,6 @@
+export const STALE_TIMES: Record<string, number> = Object.freeze({
+  FIVE_MINUTES: 1000 * 60 * 5,
+  TEN_MINUTES: 1000 * 60 * 10,
+  ONE_HOUR: 1000 * 60 * 60,
+  TWELVE_HOURS: 1000 * 60 * 60 * 12,
+});
